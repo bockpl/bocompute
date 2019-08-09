43a7e95e78ce6f4045e6e0a325aea7a90e9cb213
Seweryn Sitarski <root@ubu1604new>
2019-08-09 19:14:17 +0200

    Instalacja monit-a i zmiana sposobu startowania serwisów na monit

---
+++ monit/start_pbis.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/opt/pbis/sbin/lwsmd --syslog& echo $! > /var/run/lwsmd.pid && \
+sleep 5 && \
+domainjoin-cli join --disable ssh adm.p.lodz.pl blueocean $(cat /opt/software/Blueocean/Configs/bo_password)
