3298b0112abfdad344854359cd01fca3783eeb5a
Seweryn Sitarski <root@ubu1604new>
2019-08-14 11:28:06 +0200

    Zmiana katalogu uruchamiania jupyterhub-a

--- Dockerfile
+++ Dockerfile
@@ -38,6 +38,8 @@ ADD monit/jupyterhub.conf /etc/monit.d/
 ADD monit/start_sshd.sh /etc/monit.d/
 ADD monit/start_pbis.sh /etc/monit.d/
 ADD monit/start_sync_hosts.sh /etc/monit.d/
+
+ENV JUPYTERHUB_WORKDIR=/var/run/jupyterhub
 ADD monit/start_jupyterhub.sh /etc/monit.d/
 
 ADD start.sh /start.sh
--- monit/start_jupyterhub.sh
+++ monit/start_jupyterhub.sh
@@ -2,5 +2,7 @@
 PATH=/opt/python/python3.6.7/bin:$PATH
 LD_LIBRARY_PATH="/opt/python/python3.6.7/lib"
 export PATH LD_LIBRARY_PATH
-jupyterhub --config /jupyterhub_config.py 2>&1 > jupyterhub.log&
+mkdir -p $JUPYTERHUB_WORKDIR
+cd $JUPYTERHUB_WORKDIR
+jupyterhub --config $JUPYTERHUB_WORKDIR/jupyterhub_config.py 2>&1 > jupyterhub.log&
 exit 0
