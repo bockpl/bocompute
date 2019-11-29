e9a3817fc5c98a84acf3262fa164d2cc80752033
Seweryn Sitarski <root@ubu1604new>
2019-11-29 10:54:12 +0100

    Skasowanie pliku start_jupytherhub.sh. JupytherHub przeniesiony do boqmaster

--- monit/start_jupyterhub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PATH=/opt/python/python3.6.7/bin:$PATH
-LD_LIBRARY_PATH="/opt/python/python3.6.7/lib"
-export PATH LD_LIBRARY_PATH
-mkdir -p $JUPYTERHUB_WORKDIR
-cd $JUPYTERHUB_WORKDIR
-jupyterhub --config $JUPYTERHUB_WORKDIR/jupyterhub_config.py 2>&1 > jupyterhub.log&
-exit 0
