f8bd3e517e3c62225474105659fc2d9ba72487cf
Seweryn Sitarski <root@ubu1604new>
2020-02-19 13:12:28 +0100

    Poprawa zlej nazwy pliku pid

--- monit/start_pbis.sh
+++ monit/start_pbis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/opt/pbis/sbin/lwsmd --syslog& echo $! > /var/run/lwsmd.pid && \
+/opt/pbis/sbin/lwsmd --syslog& echo $! > /var/run/pbis.pid && \
 sleep 5 && \
 domainjoin-cli join --disable ssh adm.p.lodz.pl blueocean $(cat /opt/software/Blueocean/Configs/bo_password)
